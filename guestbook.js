24b2cf71c3294d6c5cd17c41059444b49b882e6c
syedmubashirahmed <94974742+syedmubashirahmed@users.noreply.github.com>
2022-04-18 07:10:42 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/syedmubashirahmed/serverless-guestbook.git';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
